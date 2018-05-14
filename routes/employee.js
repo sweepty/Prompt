9174e8ebf67c245bca3df5a3964e94bb3ee5b7a6
sweepty <adie0423@gmail.com>
2018-05-15 02:41:25 +0900

    [수정] employee 참여 프로젝트 리스트 추가 - 참여 프로젝트 리스트 전체를 보여주도록 함

--- routes/employee.js
+++ routes/employee.js
@@ -4,23 +4,8 @@ var mysql_dbc = require('../db/db_con')();
 var connection = mysql_dbc.init();
 var passport = require('passport'), 
   LocalStrategy = require('passport-local').Strategy;
-var bcrypt = require('bcrypt');
 
 mysql_dbc.test_open(connection);
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('employee/home', { 
-    title: 'weeellcomme',
-    user: req.params.result
-  });
-});
-router.get('/evaluation', function(req, res, next) {
-  connection.query('select * from works_on w inner join employee e where w.employee_id = e.employee_id = ?',[employee_id], function(err, result){
-    
-  });
-  res.render('employee/evaluation', {projects: projects });
-})
 
 passport.serializeUser(function (user, done) {
   done(null, user)
@@ -34,6 +19,36 @@ var isAuthenticated = function (req, res, next) {
     return next();
   res.redirect('/');
 };
+
+/* GET home page. */
+
+//프로젝트 페이지
+router.get('/', function(req, res, next) {
+  const user = req.session.user.user[0].employee_id;
+  console.log(user,'유저의 employee_id');
+  connection.query('select p.project_id, p.name, p.start_date, p.end_date, p.created_at, j.job , p.EA from works_on w join project p on w.project_id = p.project_id and w.employee_id = ? join job j on w.job_id = j.job_id',
+  [user], function(err,rows){ 
+    if (err) throw(err);
+    if (rows && rows.length > 0){
+      var info = rows;
+      // console.log(info,'프로젝트');
+      res.render('employee/home', {
+        projects: info, 
+        title: '참가한 프로젝트 전체'
+      });
+    } else{
+      res.render('employee/home',{
+        projects: [],
+        title: '참가한 프로젝트가 없습니다.'
+      })
+    }
+  });
+});
+
+router.get('/evaluation', function(req, res, next) {
+  
+})
+
 
 
 module.exports = router;
