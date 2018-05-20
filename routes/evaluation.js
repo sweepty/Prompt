e68cc2cf8d989470bfc1cdaa11e2f669007a9063
Jiye <akdhkd1678@naver.com>
2018-05-21 02:11:41 +0900

    [수정] 평가 링크 수정 및 오타 수정

--- routes/evaluation.js
+++ routes/evaluation.js
@@ -25,6 +25,7 @@ function needAuth(req, res, next) {
 
 //평가 생성 페이지 get
 router.get('/:id', needAuth, function(req, res, next){
+  var project_id = req.params.id;
   res.render('evaluation/emp_eva',{
     user: req.user,
     title: '평가 생성페이지'
@@ -36,16 +37,10 @@ router.post('/evaluation/:project_id', function(req, res, next){
   const query = 'insert into evaluation set ?';
   const query2 = 'insert into evaluation_info set ?';
 
-  var evaluation_id = req.body.evaluation_id; //평가 id
-  var evaluator_id = req.body.evaluator_id; //평가자 id
-  var evaluated_id = req.body.evaluated_id; //피평가자 id
-  var project_id = req.body.project_id; //프로젝트 id
   var type_of_evaluation = req.body.type_of_evaluation; //평가 종류
-  var created_at = req.body.created_at; //생성일시
-  var evaluation_info_id = req.body.evaluation_info_id; //평가정보 id
   var score = req.body.score; //평가점수
   var content = req.body.content; //평가내용
-  var data = {score: score, content: content};
+  var data = {type_of_evaluation: req.body.type_of_evaluation, score: score, content: content};
   console.log(data);
 });
 
