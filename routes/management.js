3fbd510bd2454b9a3650189fafc8c4ecfd395741
이승연 <adie0423@gmail.com>
2018-05-30 05:20:18 +0900

    [추가] 경영 관리 테이블 추가 - 안 돼서 데이터라도 뿌려주었음

--- routes/management.js
+++ routes/management.js
@@ -28,7 +28,7 @@ function needAuth(req, res, next) {
 router.get('/', needAuth, function(req, res, next) {
   var query = 'select year(end_date) years , quarter(end_date) quarters , month(end_date) months, sum(price) prices '+
   'from project group by ';
-
+  var query_project = 'select count(*) counter from project p ';
   if (req.user.roles.includes("management")) {
     //년도별
     connection.query(query+'years', function(err, year){
@@ -36,25 +36,42 @@ router.get('/', needAuth, function(req, res, next) {
       // 분기별
       connection.query(query+'quarters', function(err, quarter){
         if (err) throw(err);
-        //월별 콜백헬 극혐
+        //월별 콜백헬 극혐이지만 ㅎ...살려줏ㄷ
         connection.query(query+'months', function(err, month){
-          if (err) throw(err);
-          console.log(year,'연도')
-          console.log(quarter,'분기별')
-          console.log(month,'월별')
-          var price = [];
-          for(i=0;i<month.length;i++){
-            price.push(month[i].prices);
-          }
-          console.log(price);
-          res.render('admin_main',{
-            user: req.user,
-            year: year,
-            quarter: quarter,
-            month: month,
-            price: price
-            
-          })
+          //시작전
+          connection.query(query_project+'where p.start_date > now()',function(err, notyet){
+            if (err) throw(err);
+            //진행중
+            connection.query(query_project+'where p.start_date < now() and p.end_date > now()',function(err, ing){
+              if (err) throw(err);
+              //완료
+              connection.query(query_project+'where p.end_date < now()',function(err, done){
+                if (err) throw(err);
+                connection.query(query_project,function(err, all){
+                  if (err) throw(err);
+                  console.log(all,'연도')
+                  console.log(quarter,'분기별')
+                  console.log(month,'월별')
+                  var price = [];
+                  for(i=0;i<month.length;i++){
+                    price.push(month[i].prices);
+                  }
+                  console.log(price);
+                  res.render('admin_main',{
+                    user: req.user,
+                    year: year,
+                    quarter: quarter,
+                    month: month,
+                    price: price,
+                    notyet: notyet[0],
+                    ing: ing[0],
+                    done: done[0],
+                    all: all[0]
+                  })
+                });
+              });
+            });
+          });
         })
       });
     });
