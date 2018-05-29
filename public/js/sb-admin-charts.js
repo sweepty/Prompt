004eabb3cf10a46dd0fa0c451f3d3525eb121638
오충협 <tractar0213@gmail.com>
2018-05-29 11:50:31 +0900

    [추가] admin 메인 페이지 그래프 추가
    - 매출현황, 고객만족도, 프로젝트진행현황 그래프 추가

--- public/js/sb-admin-charts.js
+++ public/js/sb-admin-charts.js
@@ -52,6 +52,20 @@ var myLineChart = new Chart(ctx, {
     }
   }
 });
+
+// -- Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: ["진행중 프로젝트", "완료된 프로젝트", "마감이 1주일 이내인 프로젝트", "평가중인 프로젝트"],
+    datasets: [{
+      data: [12.21, 15.58, 11.25, 8.32],
+      backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
+    }],
+  },
+});
+
 // -- Bar Chart Example
 var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
@@ -62,7 +76,7 @@ var myLineChart = new Chart(ctx, {
       label: "Revenue",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [5.5, 6.7, 5.3, 4.1, 8.1, 9.6],
     }],
   },
   options: {
@@ -81,7 +95,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 10,
           maxTicksLimit: 5
         },
         gridLines: {
@@ -93,16 +107,4 @@ var myLineChart = new Chart(ctx, {
       display: false
     }
   }
-});
-// -- Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-  type: 'pie',
-  data: {
-    labels: ["Blue", "Red", "Yellow", "Green"],
-    datasets: [{
-      data: [12.21, 15.58, 11.25, 8.32],
-      backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
-    }],
-  },
 });
